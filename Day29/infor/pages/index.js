5ac2cad7a3d8df99258fab1965c9dceb9e79f548
Smudasay <angsay74.74@gmail.com>
2020-07-09 16:00:17 +0700

    แต่งหน้าเว็ป

--- Day29/infor/pages/index.js
+++ Day29/infor/pages/index.js
@@ -3,15 +3,14 @@ export default function Home() {
   return (
     <div className="page_home">
       <Header title="home" />
-      <h1>Home</h1>
       <h1 className="title">HELLO</h1>
-      <h1 className="title">HELLO</h1>
-    
+
+      <h1 className="cctv1"></h1>
+      <h1 className="cctv2"></h1>
 
       
     </div>
 
-
-
+     
   )
 }
