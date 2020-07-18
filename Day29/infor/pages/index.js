22b487ee41e9194743e400b65c6c07248607fcb9
smudasay <angsay74.74@gmail.com>
2020-07-18 23:43:14 +0700

    ฝึก

--- Day29/infor/pages/index.js
+++ Day29/infor/pages/index.js
@@ -1,22 +1,17 @@
 import Header from '../components/Header'
 import { Carousel } from 'antd';
+import Footer from "../components/Footer"
 export default function Home() {
   return (
     <div className="page_home">
       <Header title="home" />
       <div className="carousel" >
-        <Carousel autoplay
+        <Carousel
           style={{
             height: "500px",
             width: "800px"
           }}>
-          <div>
-            <img src="/images/cctv1.jpg"
-              style={{
-                width: "100%",
-                height: "100%"
-              }} />
-          </div>
+          
           <div>
             <img src="/images/cctv2.jpg"
               style={{
@@ -35,14 +30,21 @@ export default function Home() {
       </div>
       <div className="home1">
         <div className="home11">
-          <div classNamea="home113">
-
+          <div classNamea="home111">
+            <h1>ผลิต ออกแบบ ติดตั้งแผ่นฉนวนสำเร็จรูป</h1>
+            <p>ทางบริษัทยินดีอย่างยิ่งที่จะเข้าไปประเมินหน้างานให้แก่ลูกค้า หรือให้คำแนะนำ 
+              หากลูกค้าสนใจสร้างห้องเย็น ห้องควบคุมอุณหภูมิ กั้นไลน์ผลิต ห้องควบคุมความชื้น
+              เพราะเราคือ…โรงงานผลิตแผ่นฉนวนสำเร็จรูปกันความร้อน รับติดตั้ง 
+              พร้อมบริการหลังการขาย ครบวงจร ด้วยผลงานและประสบการณ์กว่า</p>
           </div>
-          <div classNamea="home113">
-
+          <div classNamea="home112">
+            <img src="/images/logo.png" />
           </div>
         </div>
+
+
       </div>
+      <Footer />
     </div>
   )
 }
