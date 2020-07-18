dba1c0f8eaf2f8d70dbd7448c3e7435be4a2a068
smudasay <angsay74.74@gmail.com>
2020-07-18 22:41:36 +0700

    ฝึก

--- Day29/infor/components/Header.js
+++ Day29/infor/components/Header.js
@@ -7,10 +7,14 @@ const Header = (props) => {
 
         <div>
             <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-                         integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous"></link>
+                         integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" 
+                         crossorigin="anonymous">
+            </link>
+
             <div className="header">
-                {/* <img src={img_logo}/> */}
-                <div className="logo"></div>
+                <div className="logo">
+                    <img src="/images/logo.png"/>
+                </div>
                 <div className="right_logo">
                     <div>
                         <a class="fb" href="https://www.facebook.com/profile.php?id=100004468123072" target="_blank">
--- Day29/infor/pages/index.js
+++ Day29/infor/pages/index.js
@@ -1,14 +1,48 @@
 import Header from '../components/Header'
+import { Carousel } from 'antd';
 export default function Home() {
   return (
     <div className="page_home">
       <Header title="home" />
-      <h1 className="cctv1"></h1>
-      <h1 className="cctv2"></h1>
+      <div className="carousel" >
+        <Carousel autoplay
+          style={{
+            height: "500px",
+            width: "800px"
+          }}>
+          <div>
+            <img src="/images/cctv1.jpg"
+              style={{
+                width: "100%",
+                height: "100%"
+              }} />
+          </div>
+          <div>
+            <img src="/images/cctv2.jpg"
+              style={{
+                width: "100%",
+                height: "100%"
+              }} />
+          </div>
+          <div>
+            <img src="/images/cctv3.jpg"
+              style={{
+                width: "100%",
+                height: "100%"
+              }} />
+          </div>
+        </Carousel>
+      </div>
+      <div className="home1">
+        <div className="home11">
+          <div classNamea="home113">
 
-      
+          </div>
+          <div classNamea="home113">
+
+          </div>
+        </div>
+      </div>
     </div>
-
-     
   )
 }
